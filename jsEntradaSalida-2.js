099495a3b180a7a8495a061d7d95b66a62cea7bd
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-08 20:40:00 -0300

    Ejercicio 1, 2 y 3
    alert prompt variables

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre = prompt("¿Cual es tu nombre?","Messi");
+	alert("Hola " + nombre);
+}
+
