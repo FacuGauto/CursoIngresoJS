a8084de3c250972f0f9bb6262cdef2c87150f645
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-09 19:48:50 -0300

    ej 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe;
+	
+	var importeConDescuento;
+
+	importe = document.getElementById('importe').value;
+	
+	importe = parseInt(importe);
+
+	importeConDescuento = importe - (importe * 0.25);
+
+	document.getElementById('resultado').value = importeConDescuento;
+
 	
 }
