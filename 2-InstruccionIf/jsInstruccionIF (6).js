00caa289e97aba7433ceb715747211ddf78f52af
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-09 22:15:25 -0300

    Actualizacion de ejercicios

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,19 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById('edad').value;
+
+edad = parseInt(edad);
+
+if (edad >= 18) {
+	alert("Usted es mayor de edad");
+}else if(edad < 13){
+	alert("Usted es un niño");
+}else{
+	alert("Usted es adolescente");
+}
 
 
 
