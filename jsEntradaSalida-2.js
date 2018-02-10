2df0b282479e2b358d0d6a4414c73158b2858315
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-09 21:04:57 -0300

    archivo duplicado

--- jsEntradaSalida-2.js
+++
@@ -1,8 +0,0 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
-function Mostar()
-{
-	var nombre = prompt("¿Cual es tu nombre?","Messi");
-	alert("Hola " + nombre);	
-}
-
