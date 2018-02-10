bb4d267fb7f95799ac4ca926e77829df9f06fbb6
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-09 21:37:25 -0300

    Ej 1,2 y 3 del primer parcial

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
-  
+  var importe;
+  var importeFinal;
+  var iva;
+
+  importe = prompt("Ingrese el  importe del producto: ");
+
+  importe = parseInt(importe);
+
+  iva = importe * 0.21
+
+  importeFinal = importe + iva;
+
+  document.getElementById('importeFinal').value = importeFinal;	 
+
+
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,16 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var resultado;
 
+	largo = document.getElementById('largo').value;
+	ancho = document.getElementById('ancho').value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	resultado = 3 * (2 *(largo + ancho))
+
+	alert(resultado);
 }
