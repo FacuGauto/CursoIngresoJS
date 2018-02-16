4a955232f0cab7ee4571de36548e024b2783c6c3
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-15 21:59:39 -0300

    Actualizacion de ej

--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,29 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
 
+	numeroUno = prompt("Ingrese el primer numero: ");
+	numeroDos = prompt("Ingrese el segundo numero: ");
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	suma = numeroUno + numeroDos;
+
+	if (suma == 0) 
+	{
+		document.write("CERO");
+	}
+	else
+	{
+		if (suma < 0)
+		{
+			document.write("NEGATIVO");
+		}
+		else
+		{
+			document.write("POSITIVO");
+		}
+	}
 }
