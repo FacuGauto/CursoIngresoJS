f10738180560d5cb44febb8535d56e5e3a10b7be
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-18 15:02:21 -0300

    Recuperatorio 1 ej 5

--- 9-Parciales/recuperatorio/cinco.js
+++ 9-Parciales/recuperatorio/cinco.js
@@ -1,4 +1,18 @@
 function Mostrar()
 {
+	var mes;
 
+	mes = prompt("Ingrese un mes","Enero");
+
+	switch(mes)
+	{
+		case "Enero":
+			alert("Comienza el año");
+			break;
+		case "Diciembre":
+			alert("Se vienen las fiestas");
+			break;
+		default:
+			alert("No es Enero ni Diciembre");
+	}
 }
