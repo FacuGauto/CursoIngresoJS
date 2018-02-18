65713c24368eb86ff981d7ead77d46022e84fb65
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-18 14:56:18 -0300

    Actualizacion ejercicio 5

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,25 @@
 function Mostrar()
 {
+	var diaDeLaSemana;
+	diaDeLaSemana = prompt("Ingrese un da de la semana","Lunes");
+	mensajeFinDeSemana = "Es fin de semana";
+	mensajeDiaDeLaSemana = "A trabajar";
 
+	switch(diaDeLaSemana)
+	{
+		case "Sabado":
+			alert(mensajeFinDeSemana);
+			break;
+		case "SABADO":
+			alert(mensajeFinDeSemana);
+			break;
+		case "Domingo":
+			alert(mensajeFinDeSemana);
+			break;
+		case "DOMINGO":
+			alert(mensajeFinDeSemana);
+			break;
+		default:
+			alert(mensajeDiaDeLaSemana);
+	}
 }
