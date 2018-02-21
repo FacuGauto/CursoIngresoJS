83cc153735f8f9580d54755a3c1f262489396637
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-21 20:40:24 -0300

    piedraPapelTijera1

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,80 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
-
+var eleccionUsuario;
 function comenzar()
 {
 	
+	numeroSecreto = Math.floor((Math.random() * 3) + 1);
 
+	switch(numeroSecreto)
+	{
+		case 1:
+			eleccionMaquina = "piedra";
+			break;
+		case 2:
+			eleccionMaquina = "papel";
+			break;
+		case 3:
+			eleccionMaquina = "tijera";
+			break;
+	}	
 
+	console.log(numeroSecreto);
+	console.log(eleccionMaquina);
 }//FIN DE LA FUNCIÓN
+
 function piedra()
 {
-	
+	eleccionUsuario = "piedra";
+
+	switch(eleccionMaquina)
+	{
+		case "piedra":
+			alert("Usted empató");
+			break;
+		case "tijera":
+			alert("Usted ganó");
+			break;
+		default:
+			alert("Usted perdió");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	eleccionUsuario = "papel";
 
+	switch(eleccionMaquina)
+	{
+		case "papel":
+			alert("Usted empató");
+			break;
+		case "piedra":
+			alert("Usted ganó");
+			break;
+		default:
+			alert("Usted perdió");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	eleccionUsuario = "tijera";
+
+	switch(eleccionMaquina)
+	{
+		case "tijera":
+			alert("Usted empató");
+			break;
+		case "papel":
+			alert("Usted ganó");
+			break;
+		default:
+			alert("Usted perdió");
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
