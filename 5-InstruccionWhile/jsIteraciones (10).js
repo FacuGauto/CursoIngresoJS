519fc1c462b5c706b42c77214d3a3a297595bf6b
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-23 20:19:37 -0300

    Ej 9 y10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,17 +2,68 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var negativos = 0;
+	var positivos = 0;
+	var cantidadPositivos = 0;
+	var cantidadNegativos = 0;
+	var cantidadCeros = 0;
+	var cantidadPares = 0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero = prompt("Ingrese un numero: ", "1");
+		numero = parseInt(numero);
+		console.log(numero);
+
+		while(isNaN(numero))
+		{
+			numero = prompt("Ingrese un numero: ", "1");
+			numero = parseInt(numero);
+		}
+
+		if (numero < 0) 
+		{
+			negativos = negativos + numero;
+			cantidadNegativos ++;
+		}
+		else if (numero > 0) 
+		{
+			positivos = positivos + numero;
+			cantidadPositivos ++;
+		}
+		else
+		{
+			cantidadCeros ++;
+		}
+
+		if (numero % 2 == 0) 
+		{
+			cantidadPares ++;
+		}
+
+		contador ++;
+		respuesta = prompt("Desea ingresar mas numeros?","si");
 	}
 
-
+	diferencia = positivos - negativos;
+	promedioPositivos = positivos/cantidadPositivos;
+	promedioNegativos = negativos/cantidadNegativos;
+	document.write("Suma de negativos: " + negativos + "<br>"
+	      + "Suma de positivos: " + positivos + "<br>"
+	      + "Cantidad de positivos: " + cantidadPositivos + "<br>"
+	      + "Cantidad de negativos: " + cantidadNegativos + "<br>"
+	      + "Cantidad de ceros: " + cantidadCeros + "<br>"
+	      + "Cantidad de numeros pares: " + cantidadPares + "<br>"
+	      + "Promedio de positivos: " + promedioPositivos + "<br>"
+	      + "Promedio de negativos: " + promedioNegativos + "<br>"
+	      + "Diferencia entre negativos y positivos: " + diferencia);
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,6 +1,8 @@
 function Mostrar()
 {
-
+	var numero;
+	var maximo;
+	var minimo;
 	var contador=0;
 	// declarar variables
 	
@@ -8,10 +10,45 @@ function Mostrar()
 
 	while(respuesta!='no')
 	{
-		
+		numero = prompt("Ingrese un numero:", "1");
+		numero = parseInt(numero);
+		while(isNaN(numero))
+		{
+			numero = prompt("Ingrese un numero:", "1");
+			numero = parseInt(numero);
+		}
+		if (contador == 0) 
+		{
+			maximo = numero;
+			minimo = numero;
+		}
+		else
+		{
+			if(maximo > numero)
+			{
+				maximo = maximo;
+			}
+			else
+			{
+				maximo = numero;
+			}
+			if(minimo < numero)
+			{
+				minimo = minimo;
+			}
+			else
+			{
+				minimo = numero;
+			}
+		}
+
+		contador ++;
+
+		respuesta = prompt("¿Desea ingresar mas numeros?","si");
 	
 	}
 
+	alert("El numero maximo es: " + maximo + " y el minimo es: " + minimo);
 
 
 
