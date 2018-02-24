002a2603d13d913c176d50d69e7497325aa43371
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-23 21:54:04 -0300

    seis

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,21 @@
 function Mostrar()
 {
+	var importe;
+	var dia;
+	
+	while(dia < 25)
+	{
+		importe = prompt("Ingrese el importe final del dia " + dia + ": ");
 
+		while(importe <= 0)
+		{
+			importe = prompt("Ingrese el importe final (mayor a cero) del dia " + dia + " : ");
+		}
+
+
+
+		dia ++;
+	}
+
+	
 }
