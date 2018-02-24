a83f05f18a78097cd11089f743866b10eb378f6e
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-02-23 21:53:07 -0300

    1, 2 y 3

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,20 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	var repeticiones = prompt("ingrese el número de repeticiones");
+	repeticiones = parseInt(repeticiones);
 
+	while(isNaN(repeticiones))
+	{
+		var repeticiones = prompt("ingrese el número de repeticiones");
+		repeticiones = parseInt(repeticiones);
+	}
+
+
+	for (var i = 1; i <= repeticiones; i++) 
+	{
+		alert("Hola UTN FRA");
+	}
 
 
 }//FIN DE LA FUNCIÓN
