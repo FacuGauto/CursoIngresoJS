fb331b313a5133ab04c5b5854ac069252961ccdd
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-03-02 20:32:03 -0300

    Ej 5

--- 9-Parciales/parcial2/5-switch.js
+++ 9-Parciales/parcial2/5-switch.js
@@ -3,5 +3,18 @@ function Mostrar()
 {
 	alert("Funciona 5-Switch");
 	
+	var mes;
+
+	mes = prompt("Ingrese un mes del año: ");
+
+	switch(mes)
+	{
+		case "enero":
+		case "febrero":
+			alert("Veranito!!!");
+			break;
+		default:
+			alert("Extraño Enero y Febrero");
+	}
 }
 
