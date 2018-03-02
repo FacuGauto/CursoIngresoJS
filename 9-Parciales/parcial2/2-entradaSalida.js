1937d32205acf3e979e93e25bffd91111a560a19
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-03-01 22:21:34 -0300

    Ej del 1 al 8

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -3,5 +3,19 @@ function Mostrar()
 {
 	alert("Funciona 2-EntradaSalida");
 	
+	var importe;
+	var iva;
+	var importeFinal;
+	var subtotal;
+
+	iva = 0.21;
+	importe = prompt("Ingrese el importe del producto: ");
+	importe = parseInt(importe);
+
+	subtotal = importe * iva;
+	importeFinal = importe + subtotal;
+
+	document.getElementById('importe').value = importeFinal;
+
 }
 
--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -2,6 +2,22 @@
 function Mostrar()
 {
 	alert("Funciona 3-EntradaSalida");
+
+	var largo;
+	var ancho;
+	var perimetro;
+	var resultado;
+
+	largo = document.getElementById('largo').value;
+	ancho = document.getElementById('ancho').value;
+	largo = parseInt(largo);
+	ancho = parseInt(ancho); 
+
+	perimetro = (largo + ancho) * 2;
+	console.log(perimetro);
+	resultado = perimetro * 6;
+
+	alert("Se necesitan " + resultado + " metros de alambre." );
 	
 }
 
