4c3a45c8e03569c00701f0090d41f2a3662359c2
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-03-02 19:54:55 -0300

    Add files via upload

--- 9-Parciales/recuperatorio2/8-iteraciones.js
+++ 9-Parciales/recuperatorio2/8-iteraciones.js
@@ -2,6 +2,66 @@
 function Mostrar()
 {
 	alert("Funciona 8-iteraciones");
+
+	var numero;
+	var cantPares;
+	var cantNumeros;
+	var sumaPositivos;
+	var maximo;
+	var minimo;
+	var respuesta;
+	var promedio;
+
+	sumaPositivos = 0;
+	cantNumeros = 0;
+	cantPares = 0;
+	respuesta = "si";
+
+	while(respuesta != "no")
+	{
+		numero = prompt("Ingresar numero positivo: ");
+		numero = parseInt(numero);
+
+		while(isNaN(numero) || numero <= 0)
+		{
+			numero = prompt("Ingresar numero positivo: ");
+			numero = parseInt(numero);
+		}
+
+		cantNumeros ++;
+		sumaPositivos = sumaPositivos + numero;
+		if (numero % 2 == 0) 
+		{
+			cantPares ++;
+		}
+		if (cantNumeros == 1) 
+		{
+			maximo = numero;
+			minimo = numero;
+		}
+		else
+		{
+			if (maximo < numero) 
+			{
+				maximo = numero;
+			}
+			if (minimo > numero) 
+			{
+				minimo = numero;
+			}
+		}
+		respuesta = prompt("Desea seguir ingresando numeros('no' para terminar): ");
+	}
+
+	promedio = sumaPositivos/cantNumeros;
+
+	document.write("La cantidad de numeros pares es: " + cantPares + "<br>" +
+					"El pomedio de todos los numeros ingresados es: " + promedio + "<br>" + 
+					"La suma de todos los numeros es: " + sumaPositivos + "<br>" + 
+					"El numero maximo es : " + maximo + "<br>" + 
+					"El numero minimo es: " + minimo + "<br>");
+
+
 	
 }
 
