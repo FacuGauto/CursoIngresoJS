9d82363d8471e5261ab2bae29a7713a3c2e94caf
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-03-02 21:20:01 -0300

    Ej 6 parcial 2

--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -2,6 +2,39 @@
 function Mostrar()
 {
 	alert("Funciona 6-iteraciones");
+
+	var importe;
+	var mayor;
+	var menor;
+
+	for (var i = 1; i <= 7; i++) 
+	{
+		importe = prompt("Ingreso " + i + " importe de la venta (mayor a cero):");
+		importe = parseInt(importe);
+
+		while(isNaN(importe) || importe <= 0 )
+		{
+			importe = prompt("Ingreso " + i + " importe de la venta (mayor a cero):");
+			importe = parseInt(importe);
+		}
+		if (i == 1) 
+		{
+			mayor = importe;
+			menor = importe;
+		}
+		else
+		{
+			if (mayor < importe) 
+			{
+				mayor = importe;
+			}
+			if (menor > importe) 
+			{
+				menor = importe;
+			}
+		}
+	}
 	
+	alert("Mayor importe: " + mayor + ". Menor importe: " + menor);
 }
 
