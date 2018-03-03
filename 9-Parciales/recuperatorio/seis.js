27c6f8e07cc998bacae71fbe709b84317e29370c
FacuGauto <36281520+FacuGauto@users.noreply.github.com>
2018-03-02 22:03:26 -0300

    Update

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -1,4 +1,43 @@
 function Mostrar()
 {
+	var peso;
+	var contenedorMasPesado;
+	var contenedorMenosPesado;
+	var masPesado;
+	var menosPesado;
 
+	for (var i = 1; i <= 50; i++) 
+	{
+		peso = prompt("Ingrese el peso en kilos del contenedor numero " + i + ":");
+		peso = parseInt(peso);
+
+		while(isNaN(peso) || peso <=0)
+		{
+			peso = prompt("Ingrese el peso en kilos del contenedor numero " + i + ":");
+			peso = parseInt(peso);
+		}
+
+		if(i == 1)
+		{
+			masPesado = peso;
+			contenedorMasPesado = i;
+			menosPesado = peso;
+			contenedorMenosPesado = i; 
+		}
+		else
+		{
+			if (masPesado < peso) 
+			{
+				masPesado = peso;
+				contenedorMasPesado = i;
+			}
+			if (menosPesado > peso) 
+			{
+				menosPesado = peso;
+				contenedorMenosPesado = i;
+			}
+		}
+	}
+	alert("El contenedor mas pesado es el " + contenedorMasPesado + "con " + masPesado + " kilos y el menos pesado es el "
+	+ contenedorMenosPesado + " con " + menosPesado + " kilos." );
 }
